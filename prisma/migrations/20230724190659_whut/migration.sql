381154e9263d4986a36b6504fb711e0b349ab6b1
Jan Piotrowski <piotrowski+github@gmail.com>
2023-07-25 11:05:12 +0000

    Upgrade to Prisma 5

---
+++ prisma/migrations/20230724190659_whut/migration.sql
@@ -0,0 +1,14 @@
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- RenameIndex
+ALTER INDEX "Post.title_unique" RENAME TO "Post_title_key";
+
+-- RenameIndex
+ALTER INDEX "Profile.userId_unique" RENAME TO "Profile_userId_key";
+
+-- RenameIndex
+ALTER INDEX "User.email_unique" RENAME TO "User_email_key";
